fda987d22332f8410a3abe060c1a7e1985572d8d
Karol Krizka <kkrizka@gmail.com>
2019-12-09 06:29:27 -0800

    Add support for loading multiple datafiles.

--- madpy.py
+++ madpy.py
@@ -1,6 +1,18 @@
 from math import *
+import xml.etree.ElementTree as ET
+import glob
 
 import pandas as pd
+
+def load_lhe_pattern(pattern):
+    dfs=[]
+    for path in glob.glob(pattern)[0:10]:
+        doc=ET.parse(path)
+        df=pd.DataFrame({'raw':[e for e in doc.iter('event')]})
+        df=df.raw.apply(parse_mg_raw)
+        dfs.append(df)
+    return pd.concat(dfs)
+
 
 class Particle:
     def __init__(self,pdg=None,status=None,px=0,py=0,pz=0,E=0):
@@ -34,10 +46,12 @@ class Particle:
         return spart
 
 def parse_mg_raw(raw):
+    #
+    # Parse the text content
     xsec=None
     particles=[]
-    
-    lines=raw.split('\n')
+
+    lines=raw.text.split('\n')
     for line in lines:
         line=line.strip()
         if line=='': continue
@@ -49,7 +63,17 @@ def parse_mg_raw(raw):
             p=Particle(int(parts[0]),int(parts[1]),float(parts[6]),float(parts[7]),float(parts[8]),float(parts[9]))
             particles.append(p)
 
-    return pd.Series({'xsec':xsec,'particles':particles})
+    #
+    # Parse any extra weights
+    weights=[]
+    rwgt=raw.find('rwgt')
+    if rwgt!=None:
+        for w in rwgt.iter('wgt'):
+            weights.append(float(w.text))
+    if len(weights)==0:
+        weights=[1.]
+
+    return pd.Series({'xsec':xsec,'particles':particles,'weight':weights[0]})
 
 def filter_stable(particles):
     return list(filter(lambda p: p.stable,particles))
