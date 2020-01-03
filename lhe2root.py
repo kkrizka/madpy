b7d67a453b5279fbe6c976869ed25d9b3e9f1be8
Karol Krizka <kkrizka@gmail.com>
2020-01-03 13:15:53 -0800

    Convert lhe2root to use streaming-based xml parsing to handle large number of events.

--- lhe2root.py
+++ lhe2root.py
@@ -2,7 +2,6 @@
 
 import array
 import ROOT
-import madpy
 import argparse
 import glob
 import gzip
@@ -13,7 +12,7 @@ parser.add_argument('input',type=str,help='glob pattern to use as input files')
 parser.add_argument('-o','--output',type=str,default='data.root',help='output name')
 args=parser.parse_args()
 
-MAXNPART=10
+MAXNPART=64
 
 #
 # Define the output file
@@ -26,40 +25,81 @@ t.Branch('weight',br_weight,'weight/F')
 br_nparticle=array.array('i',[0])
 t.Branch('nparticle',br_nparticle,'nparticle/I')
 
-br_particle_pt =array.array('f',[0]*MAXNPART)
-t.Branch('particle_pt',br_particle_pt,'particle_pt[nparticle]/F')
+br_particle_pt    =array.array('f',[0]*MAXNPART)
+t.Branch('particle_pt'    ,br_particle_pt    , 'particle_pt[nparticle]/F'    )
 
-br_particle_eta=array.array('f',[0]*MAXNPART)
-t.Branch('particle_eta',br_particle_eta,'particle_eta[nparticle]/F')
+br_particle_eta   =array.array('f',[0]*MAXNPART)
+t.Branch('particle_eta'   ,br_particle_eta   , 'particle_eta[nparticle]/F'   )
 
-br_particle_phi=array.array('f',[0]*MAXNPART)
-t.Branch('particle_phi',br_particle_phi,'particle_phi[nparticle]/F')
+br_particle_phi   =array.array('f',[0]*MAXNPART)
+t.Branch('particle_phi'   ,br_particle_phi   , 'particle_phi[nparticle]/F'   )
 
-br_particle_m  =array.array('f',[0]*MAXNPART)
-t.Branch('particle_m',br_particle_m,'particle_m[nparticle]/F')
+br_particle_m     =array.array('f',[0]*MAXNPART)
+t.Branch('particle_m'     ,br_particle_m     , 'particle_m[nparticle]/F'     )
 
-br_particle_pdg=array.array('i',[0]*MAXNPART)
-t.Branch('particle_pdg',br_particle_pdg,'particle_pdg[nparticle]/I')
+br_particle_pdg   =array.array('i',[0]*MAXNPART)
+t.Branch('particle_pdg'   ,br_particle_pdg   , 'particle_pdg[nparticle]/I'   )
 
 br_particle_status=array.array('i',[0]*MAXNPART)
-t.Branch('particle_status',br_particle_status,'particle_status[nparticle]/I')
+t.Branch('particle_status',br_particle_status, 'particle_status[nparticle]/I')
+
+br_weights={}
+
+#
+# the loop
+p4=ROOT.TLorentzVector()
 
 for path in glob.glob(args.input):
-    df=madpy.load_lhe_pattern(path)
-    for i,row in df.iterrows():
-        br_weight[0]=row['weight']
+    print(path)
+    for event,element in ET.iterparse(gzip.open(path) if path.endswith('gz') else path):
+        if element.tag=='event':
+            # Parse the particles in the event
+            xsec=None
+            br_nparticle[0]=0
+            
+            lines=element.text.split('\n')
+            for line in lines:
+                line=line.strip()
+                if line=='': continue
+                if line[0]=='#': continue
 
-        br_nparticle[0]=0
-        for particle in row['particles']:
-            br_particle_pt    [br_nparticle[0]]=particle.pT
-            br_particle_eta   [br_nparticle[0]]=particle.eta
-            br_particle_phi   [br_nparticle[0]]=particle.phi
-            br_particle_m     [br_nparticle[0]]=particle.M
-            br_particle_status[br_nparticle[0]]=particle.status
-            br_particle_pdg   [br_nparticle[0]]=particle.pdg
-            br_nparticle[0]+=1
+                if xsec==None: #first line
+                    xsec=float(line.split()[2])
+                else: # particle
+                    parts=line.split()
+                    pdg=int(parts[0])
+                    status=int(parts[1])
+                    px=float(parts[ 6])
+                    py=float(parts[ 7])
+                    pz=float(parts[ 8])
+                    e =float(parts[ 9])
 
-        t.Fill()
+                    p4.SetPxPyPzE(px,py,pz,e)
+                    br_particle_pt    [br_nparticle[0]]=p4.Pt ()
+                    br_particle_eta   [br_nparticle[0]]=p4.Eta()
+                    br_particle_phi   [br_nparticle[0]]=p4.Phi()
+                    br_particle_m     [br_nparticle[0]]=p4.M  ()
+                    br_particle_pdg   [br_nparticle[0]]=pdg
+                    br_particle_status[br_nparticle[0]]=status
+                    br_nparticle[0]+=1
+            br_weight[0]=xsec
+
+
+            # Parse extra weights in the event
+            rwgt=element.find('rwgt')
+            if rwgt!=None:
+                for w in rwgt.iter('wgt'):
+                    wname=w.attrib['id']
+                    if wname not in br_weights: # create a new branch if needed
+                        br_rwgt=array.array('f',[0])
+                        t.Branch('weight_{}'.format(wname),br_rwgt,'weight_{}/F'.format(wname))
+                        br_weights[wname]=br_rwgt
+
+                    br_weights[wname][0]=float(w.text)
+
+            # Fil the tree to finish the event
+            t.Fill()
+            element.clear()
 
 fh.Write()
 fh.Close()
